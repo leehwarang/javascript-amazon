6c8f0e8569910954f896ffe62169a4fa8842aa80
leehwarang <hwrng2@gmail.com>
2019-09-09 17:02:16 +0900

    setting: config url Product 버젼으로 수정

--- public/javascripts/UI_component/searchUI/config.js
+++ public/javascripts/UI_component/searchUI/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // baseUrl: "https://michelle-amazon.herokuapp.com/autocomplete?suggestion=",
-  baseUrl: "http://localhost:3000/autocomplete?suggestion="
+  baseUrl: "https://michelle-amazon.herokuapp.com/autocomplete?suggestion="
+  //   baseUrl: "http://localhost:3000/autocomplete?suggestion="
 };
 
 export default config;
