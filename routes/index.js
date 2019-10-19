ad5d90dc19769238fde3f442f3bfe3d59b926183
leehwarang <hwrng2@gmail.com>
2019-10-19 22:58:23 +0900

    fix: node에서 throw error 삭제

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,24 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
-  res.render("/index.html");
+router.get('/', (req, res, next) => {
+  res.render('/index.html');
 });
 
-router.get("/autocomplete", (req, res, next) => {
+router.get('/autocomplete', (req, res, next) => {
   const searchKeyword = req.query.suggestion;
   const jsonPath = path.join(
     __dirname,
-    "..",
-    "public",
-    "data",
+    '..',
+    'public',
+    'data',
     `${searchKeyword}.json`
   );
-  fs.readFile(jsonPath, (err, data) => {
-    if (err) throw err;
-    const jsonData = JSON.parse(data);
-    return res.json(jsonData);
-  });
+
+  return res.json(JSON.parse(fs.readFileSync(jsonPath)));
 });
 module.exports = router;
